593aabd86c272669890742f90698f407c71e4975
RaulTeles <telesraul@gmail.com>
2025-01-02 22:14:51 -0300

    finish Up_Downcasting

--- Up_Downcast/src/Main.java
+++ Up_Downcast/src/Main.java
@@ -1,3 +1,7 @@
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -6,11 +10,31 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        Account acc = new Account(01,"Raul",2.700);
+        BusinessAccount bcc = new BusinessAccount(102,"Ladjane",1.500,200.0);
 
+//      UPCASTING
 
+        Account acc1 = bcc;
+        Account acc2 = new BusinessAccount(103,"Teles",2.000,300.0);
+        Account acc3 = new SavingsAccount(104,"Silva",1.300,0.15);
 
+//       DOWNCASTING
 
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100.0);
+//        BusinessAccount acc5 = (BusinessAcount)acc3;
+        if (acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
 
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("UPDATE!");
+        }
 
         sc.close();
     }
